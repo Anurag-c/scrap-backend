b49d2a29b174a46d8e1c23612c4f7f9e00376a45
Nampally Anurag <anurag.nampally@gmail.com>
2022-04-05 00:12:07 +0530

    vnr1923

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userController = require("./../controllers/userController");
+const vnr1923Controller = require("./../controllers/vnr1923Controller");
 
 const router = express.Router();
 
